b850a7460356a6ae61b4159883e26ac664566f2f
bezhushko <151675558+bezhushko@users.noreply.github.com>
2024-01-10 20:15:24 +0100

    ok

---
+++ js/scripst01.js
@@ -0,0 +1,13 @@
+const changeH1Text= document.querySelector('#textH1');
+textH1.textContent = "il tuo smartphone"
+
+const changeBackgroundColor= document.querySelector ('#body');
+body.style.backgroundColor= "pink" 
+
+const changeAddressText = document.querySelector ('#addressText');
+addressText.textContent ="Via Marconi 113 Roma"
+
+
+
+
+    
